d88d38184aaaa6e047fc2a01b8e46c9e265ef46e
Mariela <marims1011@gmail.com>
2019-06-14 23:05:50 -0600

    Se creo la vista2

--- app/src/main/java/com/example/examenfinal/MainActivity.java
+++ app/src/main/java/com/example/examenfinal/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.examenfinal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,6 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +24,13 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, RegistrarAlumnoActivity.class);
+                startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
             }
         });
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/example/examenfinal/RegistrarAlumnoActivity.java
@@ -0,0 +1,24 @@
+package com.example.examenfinal;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegistrarAlumnoActivity extends AppCompatActivity {
+    private static final int REQUEST_IMAGE_CAPTURE = 101;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_alumno);
+    }
+
+
+    public void tomarFoto(View view) {
+        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(imageTakeIntent.resolveActivity(getPackageManager()) !=null) {
+            startActivityForResult(imageTakeIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+}
